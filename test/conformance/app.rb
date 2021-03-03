722305e7514bd60afe464ca89da784f5f35cefb2
Michael Traver <mtraver@users.noreply.github.com>
2021-03-03 11:18:53 -0800

    chore: Run conformance tests during CI (#88)
    * Run conformance tests
    
    * Conformance tests: add actions/setup-go@v2, use ruby/setup-ruby@v1
    
    * Conformance tests: fix image tag
    
    * Conformance tests: create Gemfile.lock in testdata setup script
    
    * Conformance tests: File.open instead of File.write
    
    * Conformance tests: don't use buildpacks
    
    * Conformance test: remove working-directory
    
    * Conformance tests: use full path to output file
    
    * Conformance tests: double quote commands
    
    Python does it. See if it solves the mysterious issue I'm seeing where
    I can't write function_output.json?
    
    * Conformance tests: bundle install instead of setup script
    
    * Conformance tests: set output file
    
    * Conformance tests: move app source, clean up unnecessary stuff
    
    * Conformance tests: don't set output file
    
    * Conformance tests: fix lint findings
    
    * Conformance tests: matrix test Ruby 2.5, 2.6, and 2.7
    
    * Conformance tests: address review comments
    
    * Conformance tests: fully specify conformance test version again

---
+++ test/conformance/app.rb
@@ -0,0 +1,22 @@
+require "json"
+require "functions_framework"
+require "cloud_events/json_format"
+
+OUTPUT_FILE = "function_output.json".freeze
+
+FunctionsFramework.http "http_func" do |request|
+  File.write OUTPUT_FILE, request.body.read
+  "ok"
+end
+
+FunctionsFramework.cloud_event "cloudevent_func" do |event|
+  # The conformance tests expect that JSON data is marshaled as a JSON object,
+  # not a string. If the data is JSON but is represented as a string, parse it
+  # so that later it's marshaled as expected.
+  if event.data_content_type == CloudEvents::ContentType.new("application/json") && event.data.instance_of?(String)
+    event = event.with data: JSON.parse(event.data)
+  end
+
+  json_format = CloudEvents::JsonFormat.new
+  File.write OUTPUT_FILE, json_format.encode(event)
+end
